4e03e7084a817ec16616256fcb26926d9a6c90a9
Saksham <chauhanssaksham@gmail.com>
2020-04-12 21:48:09 +0530

    Added home page stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,14 @@
 import React, {Fragment} from 'react';
-import {Redirect, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './styles/App.scss';
 // import './styles/materialize/js/bin/materialize.js';
-import LoginForm from './components/Auth/LoginForm'
-import SignUpForm from './components/Auth/SignUpForm'
+import HomePage from './components/Layout/HomePage'
 
 function App() {
   return (
-    <Router>
     <Fragment>
     {/* <Redirect to='/login' /> */}
-        <div className="row main">
-            <div className="col s12 l7 grey lighten-2"><div><h1>Hello</h1></div></div>
-            <div className="col s12 l5 right-section">
-            <Switch>
-                <Route exact path='/' component={LoginForm} />
-                <Route exact path='/signup' component={SignUpForm} />
-            </Switch>
-            </div>
-        </div>
+        <HomePage/>
     </Fragment>
-    </Router>
   );
 }
 
---
+++ client/src/components/Layout/HomePage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Redirect, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginForm from '../Auth/LoginForm'
+import SignUpForm from '../Auth/SignUpForm'
+
+const HomePage = () => {
+    return (
+        <Router>
+        <div className="row main">
+            <div className="col s12 l7 grey lighten-2">
+            <div className="home-main">
+                <h3>My Year Diary</h3>
+                <p class="flow-text">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+                        <li>Quia porro exercitationem incidunt cumque facere accusantium reprehenderit.</li>
+                        <li>Illo voluptatum modi magnam? Ratione veniam nostrum illum?</li>
+                        <li>Optio molestiae illo voluptatibus sapiente voluptates provident voluptatem?</li>
+                        <li>Asperiores modi obcaecati quod facilis! Aperiam, vel non!</li>
+                        <li>Modi numquam sint et dolores. Ratione, laborum sunt.</li>
+                        <li>Repellendus blanditiis aliquam vitae suscipit atque eius itaque.</li>
+                    </ul>
+                </p>
+            </div>
+            </div>
+            <div className="col s12 l5 right-section">
+            <Switch>
+                <Route exact path='/' component={LoginForm} />
+                <Route exact path='/signup' component={SignUpForm} />
+            </Switch>
+            </div>
+        </div>
+        </Router>
+    )
+}
+
+export default HomePage
